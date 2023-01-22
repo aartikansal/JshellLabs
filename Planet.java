02365aef7702f135f2a8aa55fb06317d249b1cf4
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-01-21 20:06:38 -0500

    Add files via upload

---
+++ Planet.java
@@ -0,0 +1,12 @@
+class Planet{
+           void revolve(){
+           System.out.println("Revolve");
+}
+
+ public static void main(String[] args){
+
+Planet venus = new Planet();
+venus.revolve();
+}
+
+       }
